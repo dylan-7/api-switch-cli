6d0e97681ba4f77f1738f40905d4cd124b7884ae
dylan-7 <wuxiewang@gmail.com>
2019-12-10 16:11:42 +0800

    Added: subport websocket

--- README.md
+++ README.md
@@ -8,6 +8,25 @@ yarn add api-switch-cli -D
 ```
 
 ## Usage
+// package.json
 
+```js
+  "api-switch-cli": {
+    "rootFile": ".env",
+    "globals": {
+      "dev": {
+        "http": "https://xxx.net",
+        "ws": "wss://ws.xxx.net:1000"
+      },
+      "test": {
+        "http": "https://xxx.net",
+        "ws": "wss://ws.xxx.net:1000"
+      }
+    }
+  }
+```
 
 ## Options
+* rootFile API file
+
+* globals local develop API
--- bin/index.js
+++ bin/index.js
@@ -12,7 +12,7 @@
  var inquirer = require('inquirer');
  var chalk = require('chalk');
  var boxen = require('boxen');
- 
+
  /**
     *
     * @param {Object} option {dev: 'https:www.xx.com'}
@@ -21,53 +21,39 @@
  var apiSwitchCli = function(action) {
    fs.readFile(path.resolve('package.json'), 'utf-8',
    function(error, data) {
+     var isProd = process.env.NODE_ENV === 'production';
      var packageJson = JSON.parse(data);
      var config = packageJson['api-switch-cli'];
      var configKeys = [];
- 
-     console.log(boxen(chalk.magenta(packageJson.name + '\n' + packageJson.description), {
+
+     console.log(boxen(chalk.magenta(process.env.npm_package_name + '\n' + process.env.npm_package_description), {
        padding: 4,
        margin: 2
      }));
- 
+
      /**
-       * @description Domain of production mode readfile
+       * @description command cli
        * @param {Function} fn function(url) { command }
-       * @param {String} url domain
+       * @param {String} url api url
        */
-     var asyncAction = function(fn, url) {
-       var command = ['cross-env', 'NODE_ENV=development'].concat(fn(url));
- 
+     var asyncAction = function(fn, url, socket) {
+       var command = ['cross-env', 'NODE_ENV=development'].concat(fn(url, socket));
+
        Spawn.sync('yarn', command, {
          stdio: 'inherit'
        });
      }
- 
-     var genarator = function(option, n) {
-       var selected = n;
- 
-       if (!selected) {
-         return;
-       }
- 
-       if (process.env.NODE_ENV !== 'production' && !option[selected]) {
-         console.error('❌ api-switch-cli:', 'The selected ' + chalk.bgRed(selected) + 
-          ' is not found. Please confirm that you have configured it');
-         return;
-       }
- 
-       var domain = option ? option[selected] : '';
- 
-       if (process.env.NODE_ENV !== 'production' && !option) {
-         console.error('❌ api-switch-cli:', 'must pass in an object!');
-         return;
-       }
-       if (!option) {
-         console.error('❌ api-switch-cli:', 'Please pass in an API object!');
-         return;
-       }
- 
-       if (process.env.NODE_ENV === 'production') {
+
+     /**
+     * @description Domain of production mode readfile
+     * @param {Object} globals domain { dev: { http: 'http://www.xxx.com', ws: 'ws:xx.com:1000' } }
+     * @param {String} n selected url
+     */
+     var genarator = function(option) {
+       var http = option.http;
+       var socket = option.ws;
+
+       if (isProd) {
          if (!config.rootFile) {
            console.error('❌ api-switch-cli:', 'Please configure api-switch-cli [rootFile]!');
            return;
@@ -75,44 +61,46 @@
          fs.readFile(path.resolve(config.rootFile), 'utf-8',
          function(error, data) {
            var message = '';
- 
+           var urls = data.split('\n');
+           var httpUrl = '';
+           var socketUrl = '';
+
            if (error) {
              message = error;
-           } else {
-             try {
-               var index = data.lastIndexOf('API_HOST=\"');
- 
-               if (~index) {
-                 domain = JSON.parse(data.substring(index + 9, data.indexOf('\"\n') + 1));
-                 isSelect = 'yes';
-                 asyncAction(action, domain);
-               } else {
-                 message = 'API_HOST not found!';
-               }
-             } catch(err) {
-               message = err;
-             }
+            } else if (!data.trim()) {
+              message = 'Please preset your API address.';
+            } else {
+              if (urls[0].split('=')[0] === 'HOST') {
+                httpUrl = JSON.parse(urls[0].split('=')[0] === 'HOST' ? urls[0].split('=')[1] : '');
+              }
+              if (urls[1].split('=')[0] === 'WS') {
+                socketUrl = JSON.parse(urls[1].split('=')[0] === 'WS' ? urls[1].split('=')[1] : '');
+              }
+              if (httpUrl || socketUrl) {
+                try {
+                  asyncAction(action, httpUrl, socketUrl);
+                } catch(err) {
+                  message = err;
+                }
+              } else {
+                message = 'HTTP or WS field not found.';
+              }
            }
- 
+
            if (message) {
              console.error('❌ api-switch-cli:', message);
            }
          });
        } else {
-         if (!domain) {
-           console.error('❌ api-switch-cli:', 'Please pass in an API object!');
-           return;
-         }
- 
-         asyncAction(action, domain);
+         asyncAction(action, http, socket);
        }
      }
- 
-     if (process.env.NODE_ENV !== 'production') {
-       if (config.url && JSON.stringify(config.url) !== '{}') {
-         configKeys = Object.keys(config.url);
+
+     if (!isProd) {
+       if (config.globals && JSON.stringify(config.globals) !== '{}') {
+         configKeys = Object.keys(config.globals);
        } else {
-         console.error('❌ api-switch-cli:', 'Please configure api-switch-cli [url]!');
+         console.error('❌ api-switch-cli:', 'Please configure api-switch-cli [globals]!');
          return;
        }
        inquirer.prompt([{
@@ -124,15 +112,20 @@
            return val.toLowerCase();
          }
        }]).then(answers => {
-         genarator(config.url, answers.name);
+         genarator({
+           http: process.env['npm_package_api_switch_cli_globals_' + answers.name + '_http'],
+           ws: process.env['npm_package_api_switch_cli_globals_' + answers.name + '_ws']
+          });
        });
      } else {
-       genarator(config.url, 'pro');
+       genarator({
+         http: '',
+         ws: ''
+       });
      }
    });
- 
+
    return;
  }
- 
+
  module.exports = apiSwitchCli;
- 
